664b55828d326fc226ddc910d5ac0393d08c0731
Jonathan Simmons <john_simmons2@baylor.edu>
2019-11-18 01:48:15 -0600

    Got the tutorial up to 1:41:00 rendering a blue thing from shader

--- engine/src/main/java/level/Level.java
+++ engine/src/main/java/level/Level.java
@@ -11,8 +11,8 @@ public class Level {
         float[] vertices = new float[] {
             -10.0f, -10.0f * 9.0f / 16.0f, 0.0f,
             -10.0f, 10.0f * 9.0f / 16.0f, 0.0f,
-            0.0f, -10.0f * 9.0f / 16.0f, 0.0f,
-            0.0f, 10.0f * 9.0f / 16.0f, 0.0f
+            0.0f, 10.0f * 9.0f / 16.0f, 0.0f,
+            0.0f, -10.0f * 9.0f / 16.0f, 0.0f
         };
 
         byte[] indices = new byte[] {
@@ -24,7 +24,7 @@ public class Level {
             0, 1,
             0, 0,
             1, 0,
-            1, 0
+            1, 1
         };
 
         background = new VertexArray(vertices, indices, tcs);
